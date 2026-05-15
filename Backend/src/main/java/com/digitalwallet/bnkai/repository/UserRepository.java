0067991ac5d190ca5099d199789057338857d6e9
anand gopal <gopalanand783@gmail.com>
2026-05-15 14:40:00 +0530

    Add User And Address Repo And UserDashboard Projection

---
+++ Backend/src/main/java/com/digitalwallet/bnkai/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.digitalwallet.bnkai.repository;
+
+import com.digitalwallet.bnkai.entity.User;
+import com.digitalwallet.bnkai.projection.UserDashboardProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource(
+        path = "users",
+        excerptProjection =
+                UserDashboardProjection.class
+)
+public interface UserRepository
+        extends JpaRepository<User, Integer> {
+
+    boolean existsByEmail(
+            String email
+    );
+
+    Optional<User> findByEmail(
+            String email
+    );
+}
