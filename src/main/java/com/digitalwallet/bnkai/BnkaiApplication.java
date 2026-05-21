6d74aca2ce422304cc37658109236ced644c2103
SwaritSharma <swarit.developer@gmail.com>
2026-05-21 10:56:07 +0530

    chore: set application-wide timezone to Asia/Kolkata in properties and initialization logic

--- src/main/java/com/digitalwallet/bnkai/BnkaiApplication.java
+++ src/main/java/com/digitalwallet/bnkai/BnkaiApplication.java
@@ -3,10 +3,19 @@ package com.digitalwallet.bnkai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BnkaiApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
+
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(BnkaiApplication.class, args);
 	}
 
